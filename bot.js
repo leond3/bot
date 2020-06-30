6d7ab5aa17d1e25fe511bf5af17a9d665e9ffe77
leond3 <59891470+leond3@users.noreply.github.com>
2020-06-30 23:28:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ bot.once('ready', () => {
 });
 
 bot.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+	if (message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
