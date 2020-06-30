f2254e46b50a3b5de19a22f6820dc39d495a8baf
leond3 <59891470+leond3@users.noreply.github.com>
2020-06-30 22:25:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,62 +10,13 @@ client.once('ready', ready => {
 });
 
 client.on('message', message => {
-	let blacklisted = ["randomgeneratedblacklistedword"];
-	for (var i in blacklisted) {
-		if (message.content.toLowerCase().includes(blacklisted[i].toLowerCase()) && !message.author.bot) {
-			message.delete(200);
-			message.channel.send(":warning: " + message.member.user.tag + " you cannot use blacklisted words or characters in your message:\n||`" + message.content + "`||").then(msg => {msg.delete(12000)});
-		}
-	}
-
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 	
 	if(message.channel.name == "verify") {
 		message.delete(1000);
 	}
-	else if(message.channel.name == "giveaways") {
-		if (command === 'gstart' && message.member.roles.find(r => r.name === "Giveaways") && !message.author.bot) {
-			message.delete(100000);
-		}
-		else if (command === 'gstart' && !message.member.roles.find(r => r.name === "Giveaways") && !message.author.bot) {
-			message.delete(100000);
-		}
-		else if (!message.member.roles.find(r => r.name === "Giveaway") && !message.author.bot) {
-			message.delete(100000);
-		}
-	}
-	else if(message.channel.name == "music") {
-		if (message.content.startsWith(prefix) && !message.author.bot) {
-			message.delete(4000);
-		}
-		else if (message.member.roles.find(r => r.name.toLowerCase() === "music")) {
-			message.delete(10000);
-		}
-		else if (!message.content.startsWith(prefix) && !message.author.bot) {
-			message.delete();
-			message.channel.send(":no_entry: **You can't chat in this channel, try: '!help'.**").then(msg => {msg.delete(4000)});
-		}
-	}
-	else if(message.channel.name == "commands") {
-		if (command === 'cf') {
-			var cf = Array(2);
-			cf[1] = "Heads";
-			cf[2] = "Tails";
-			
-			var coinflip = getRandomInt(1, 3);
-			if (coinflip === 1) { message.channel.send(cf[1]).then(msg => {msg.delete(100000)}); }
-			if (coinflip === 2) { message.channel.send(cf[2]).then(msg => {msg.delete(100000)}); }
-			message.delete(10000);
-		}		
-	}
-	else if(message.channel.name == "trail-vote") {
-		if (command === 'tv') {
-			const mention = message.mentions.members.first();
-			message.channel.send("What did you think of " + mention + " and his/her **Trail Status**, and should this status be kept?\n👍 yes\n👎 no").then(() => message.react('👍')).then(() => message.react('👎'));
-		}
-		message.delete(1000);
-	}
+	
 });
 
 client.login(process.env.token);
