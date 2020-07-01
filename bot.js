9b4bb42ccc4de77149c95ba920f52dce0f67f5d8
leond3 <59891470+leond3@users.noreply.github.com>
2020-07-01 10:48:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,49 +17,44 @@ bot.on('message', message => {
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if(message.channel.name == "verify") {
-		message.delete({timeout:1000});
-	}
-	else if(message.channel.name == "apply-for-rank") {
+	if(message.channel.name == "verify" || message.channel.name == "apply-for-rank") {
 		message.delete({timeout:1000});
 	}
 	else if(message.channel.name == "giveaways") {
-			if (command === 'gstart' && message.member.roles.find(r => r.name === "Giveaways") && !message.author.bot) {
-				message.delete({timeout:100000});
+		if (command === 'gstart' && message.member.roles.find(r => r.name === "Giveaways") && !message.author.bot) {
+			message.delete({timeout:100000});
 			}
-			else if (command === 'gstart' && !message.member.roles.find(r => r.name === "Giveaways") && !message.author.bot) {
-				message.delete({timeout:100000});
+		else if (command === 'gstart' && !message.member.roles.find(r => r.name === "Giveaways") && !message.author.bot) {
+			message.delete({timeout:100000});
 			}
-			else if (!message.member.roles.find(r => r.name === "Giveaway") && !message.author.bot) {
-				message.delete({timeout:100000});
-			}
+		else if (!message.member.roles.find(r => r.name === "Giveaway") && !message.author.bot) {
+			message.delete({timeout:100000});
 		}
-		else if(message.channel.name == "music") {
-			if (message.content.startsWith(prefix) && !message.author.bot) {
-				message.delete({timeout:100000});
-			}
-			else if (message.member.roles.find(r => r.name.toLowerCase() === "music")) {
-				message.delete({timeout:1000});
-			}
-			else if (!message.content.startsWith(prefix) && !message.author.bot) {
-				message.delete({timeout:1000});
-				message.channel.send(":no_entry: **You can't chat in this channel, try: '!help'.**").then(msg => {msg.delete(4000)});
-			}
+	}
+	else if(message.channel.name == "music") {
+		if (message.content.startsWith(prefix) && !message.author.bot) {
+			message.delete({timeout:100000});
 		}
-		else if(message.channel.name == "commands") {
-			if (command === 'help') {
-				 message.channel.send("**Commands help list:**\n- !help\n- !hg!link [in-game username]\n- !coinflip (cf)").then(msg => {msg.delete({timeout:10000})});
-			}
-			else if (command === 'coinflip' || command === 'cf') {
-				var cf = Array(2);
-				cf[1] = "Heads";
-				cf[2] = "Tails";
-				
-				var coinflip = getRandomInt(1, 3);
-				if (coinflip === 1) { message.channel.send(cf[1]).then(msg => {msg.delete({timeout:10000})}); }
-				if (coinflip === 2) { message.channel.send(cf[2]).then(msg => {msg.delete({timeout:10000})}); }
-			}
+		else if (message.member.roles.find(r => r.name.toLowerCase() === "music")) {
 			message.delete({timeout:1000});
+		}
+		else if (!message.content.startsWith(prefix) && !message.author.bot) {
+			message.delete({timeout:1000});
+			message.channel.send(":no_entry: **You can't chat in this channel, try: '!help'.**").then(msg => {msg.delete(4000)});
+		}
+	}
+	else if(message.channel.name == "commands") {
+		if (command === 'help') {
+			 message.channel.send("**Commands help list:**\n- !help\n- !hg!link [in-game username]\n- !coinflip (cf)").then(msg => {msg.delete({timeout:10000})});
+		}
+		else if (command === 'coinflip' || command === 'cf') {
+			var cf = Array(2);
+			cf[1] = "Heads";
+			cf[2] = "Tails";
+			
+			var coinflip = getRandomInt(1, 3);
+			if (coinflip === 1) { message.channel.send(cf[1]).then(msg => {msg.delete({timeout:10000})}); }
+			if (coinflip === 2) { message.channel.send(cf[2]).then(msg => {msg.delete({timeout:10000})}); }
 		}
 		else if(message.channel.name == "trail-vote") {
 			if (command === 'tv') {
@@ -74,6 +69,13 @@ bot.on('message', message => {
 			}
 			message.delete({timeout:1000});
 		}
+	}
+	
+	function getRandomInt(min, max) {
+	 	min = Math.ceil(min);
+	  	max = Math.floor(max);
+	 	return Math.floor(Math.random() * (max - min)) + min;
+	}
 });
 
 bot.login(process.env.token);
